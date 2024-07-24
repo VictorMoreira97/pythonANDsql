cc286d45b6f904e3d173964d79b370d51689fa2e
Victor Chaves Moreira <101597103+VictorMoreira97@users.noreply.github.com>
2024-07-24 09:33:28 -0300

    Update pyMysqlAula.py

--- pyMysqlAula.py
+++ pyMysqlAula.py
@@ -2,9 +2,11 @@ import os
 import dotenv
 import pymysql
 
+TABLE_NAME = 'customers'
+
 dotenv.load_dotenv()
 
-connection - pymysql.connect(
+connection = pymysql.connect(
     host='localhost',
     user='usurio',
     password='senha',
@@ -13,6 +15,7 @@ connection - pymysql.connect(
     user=os.environ['MYSQL_USER'],
     password=os.environ['MYSQL_PASSWORD'],
     database=os.environ['MYSQL_DATABASE'],
+    charset='utf8mb4'
 )
 
 with connection.cursor() as cursor:
@@ -20,9 +23,19 @@ with connection.cursor() as cursor:
     with connection.cursor() as cursor:
         cursor.execute(
             'CREATE TABLE customers('
+            f'CREATE TABLE IF NOT EXISTIS {TABLE_NAME}'
             'id INT NOT NULL AUTO_INCREMENT, '
             'nome VARCHAR(50) NOT NULL, '
             'idade INT NOT NULL, '
             'PRIMARY KEY (id)'
         )
     print(cursor)
+    connection.commit()
+
+with connection.cursor() as cursor:
+    result = cursor.execute(
+        f'INSERT INTO {TABLE_NAME}'
+        '(nome, idade) VALUES ("Luiz", 25)'
+    )
+    print(result)
+connection.commit()
