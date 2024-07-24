a9fe78ccd92c152af6e65485cfa5f814260ac5b7
Victor Chaves Moreira <101597103+VictorMoreira97@users.noreply.github.com>
2024-07-24 15:43:37 -0300

    m

--- pyMysqlAula.py
+++ pyMysqlAula.py
@@ -33,9 +33,61 @@ with connection.cursor() as cursor:
     connection.commit()
 
 with connection.cursor() as cursor:
-    result = cursor.execute(
+    sql = (
         f'INSERT INTO {TABLE_NAME}'
-        '(nome, idade) VALUES ("Luiz", 25)'
+        '(nome, idade)'
+        'VALUES '
+        '(%s, %s)'
     )
+    data2 = (
+        {"name": "Siri", "age": 22, },
+        {"name": "Helena", "age": 15, },
+    )
+    result = cursor.execute(sql, data2)
+    print(sql)
+    print(data2)
     print(result)
 connection.commit()
+
+with connection.cursor() as cursor:
+        sql = (
+             f'SELECT * FROM {TABLE_NAME} '
+        )
+        cursor.execute(sql)
+
+data4 = (
+     ("Siri", 22, ),
+     ("Helena", 15, ),
+     ("Luiz", 18, ),
+)
+result = cursor.executemany(sql, data4)
+
+with connection.cursor() as cursor:
+     menor_id = int(input('Digite o menor id: '))
+     maior_id = int(input('Digite o maior id: '))
+
+     sql = (
+          f'SELECT * FROM {TABLE_NAME}'
+          'WHERE id BETWEEN %s AND %s'
+     )
+     cursor.execute(sql)
+
+     cursor.execute(sql, (menor_id, maior_id))
+     print(cursor.mogrify(sql, (menor_id, maior_id)))
+     data5 = cursor.fetchall()
+
+connection.commit()
+
+with connection.cursor() as cursor:
+     id_recebido = input('Digite um id: ')
+     coluna = 'id'
+     sql = (
+          f'SELECT * FROM {TABLE_NAME}'
+          f'WHERE {coluna} = %s '
+     )
+     cursor.execute(sql, (id_recebido,))
+     print(cursor.mogrify(sql, (id_recebido,)))
+     data5 = cursor.fetchall()
+
+     for row in data5:
+          print(row)
